4085e5187635c7f45ebede54ff8b8111a7d27729
Caio Abe <caioabe68@gmail.com>
2025-08-21 23:13:47 -0300

    feat: Adicionar herança do Smartphone e sobescrever método InstalarAplicativo

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // TODO: Herdar da classe "Smartphone" (CONCLUÍDO!)
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Sobrescrever o método "InstalarAplicativo" (CONCLUÍDO!)
+        public void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado no Nokia!");
+        }
     }
 }
