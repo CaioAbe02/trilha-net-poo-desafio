e5df083734adafcd08036c1a54335418e6d158a3
Caio Abe <caioabe68@gmail.com>
2025-08-21 23:22:41 -0300

    feat: Adicionar herança do Smartphone e sobescrever método InstalarAplicativo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // TODO: Herdar da classe "Smartphone" (CONCLUÍDO!)
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Sobrescrever o método "InstalarAplicativo" (CONCLUÍDO!)
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado no Iphone!");
+        }
     }
 }
