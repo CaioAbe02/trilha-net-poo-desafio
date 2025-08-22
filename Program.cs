0ec7032208a74ff5f28d2950222153408bfecf2c
Caio Abe <caioabe68@gmail.com>
2025-08-21 23:49:05 -0300

    feat: testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone (CONCLUÍDO!)
+Console.WriteLine("-== NOKIA =--");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Tijolo", imei: "1111", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n-== IPhone ==-");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "IPhone 16", imei: "2222", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
