eb75b34d4d149d3acd9bd32e72979d3e3d2de571
Caio Abe <caioabe68@gmail.com>
2025-08-21 23:49:00 -0300

    fix: Construtores

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,6 +3,7 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone" (CONCLUÍDO!)
     public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) {}
         // TODO: Sobrescrever o método "InstalarAplicativo" (CONCLUÍDO!)
         public override void InstalarAplicativo(string nomeApp)
         {
